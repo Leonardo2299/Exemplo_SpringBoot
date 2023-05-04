05d0f66edcfb4a63568ffc10b853345c92cb6394
Léo Almeida <leonardo2299@gmail.com>
2023-05-04 11:00:08 -0300

    insercao do banco de dados h2

--- demo/src/main/java/com/leoteste00/demo/entities/Departament.java
+++ demo/src/main/java/com/leoteste00/demo/entities/Departament.java
@@ -1,7 +1,17 @@
 package com.leoteste00.demo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_department")
 public class Departament {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
